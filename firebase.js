f019f5b9ec9152bbd3d61202df9ce3023cd3a518
hanzaz-abdelaziz <119833953+hanzaz-abdelaziz@users.noreply.github.com>
2022-12-11 23:08:08 +0100

    export firebase auth and firestore

--- firebase.js
+++ firebase.js
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
     
@@ -12,3 +14,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const db = getFirestore(app);
+
+export {auth, db};
